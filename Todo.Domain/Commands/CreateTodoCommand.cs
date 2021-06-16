57bc688e9bd38691b0c2a412071e856302df6bac
Robinson Soares <soares.robinson@gmail.com>
2021-06-16 10:16:26 -0300

    Creating Command Result

--- Todo.Domain/Commands/CreateTodoCommand.cs
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -1,12 +1,42 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand
+    public class CreateTodoCommand : Notifiable, Icommand
     {
+        public CreateTodoCommand() { }
+
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+
         public string Title { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
 
+        public void Validate()
+        {
+            // Errado
+            /* if (Title.Length < 4)
+                throw new Exception("Titulo inválido");
+
+            if (User.Length < 4)
+                throw new Exception("Usuário inválido"); */
+
+            //Design by Contracts
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa!")
+                    .HasMinLen(User, 6, "User", "Usuário inválido!")
+            );
+
+        }
     }
 }
---
+++ Todo.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,16 @@
+namespace Todo.Domain.Commands.Contracts
+{
+    public class GenericCommandResult : ICommandResult
+    {
+        public GenericCommandResult(bool success, string messsage, object data)
+        {
+            Success = success;
+            Messsage = messsage;
+            Data = data;
+        }
+
+        public bool Success { get; set; }
+        public string Messsage { get; set; }
+        public object Data { get; set; }
+    }
+}
