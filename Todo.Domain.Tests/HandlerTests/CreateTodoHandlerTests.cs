78f8ce4c255ac91394b4b24b5cdd11316c57333c
Robinson Soares <soares.robinson@gmail.com>
2021-06-18 09:45:19 -0300

    Creating FakeRepository

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            var command = new CreateTodoCommand("", "", DateTime.Now);
+            var handler = new TodoHandler(new FakeTodoRepository());
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+            Assert.Fail();
+        }
+    }
+}
