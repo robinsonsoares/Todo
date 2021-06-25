03a253b6069d4aea68a5e81916a7e287e00738fa
Robinson Soares <soares.robinson@gmail.com>
2021-06-25 09:44:45 -0300

    Creating Routers

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
@@ -20,6 +21,111 @@ namespace Todo.Api.Controllers
         )
         {
             return repository.GetAll("brianbob");
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAllDone("brianbob");
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAllUndone("brianbob");
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod(
+                "brianbob",
+                DateTime.Now.Date,
+                true
+            );
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetInactiveForToday(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod(
+                "brianbob",
+                DateTime.Now.Date,
+                false
+            );
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod(
+                "brianbob",
+                DateTime.Now.AddDays(1),
+                true
+            );
+        }
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod(
+                "brianbob",
+                DateTime.Now.AddDays(1),
+                false
+            );
+        }
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update(
+            [FromBody] UpdateTodoCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            command.User = "brianbob";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone(
+            [FromBody] MarkTodoAsDoneCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            // command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            command.User = "brianbob";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone(
+            [FromBody] MarkTodoAsUndoneCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            //command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            command.User = "brianbob";
+            return (GenericCommandResult)handler.Handle(command);
         }
 
         [Route("")]
--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -42,8 +42,8 @@ namespace Todo.Domain.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo.Domain.Api v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo.Domain.Api v1"));
             }
 
             app.UseHttpsRedirection();
